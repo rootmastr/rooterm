2cfa3b8e8fc37505ad36cd250bc6523e37984db4
rootmastr <suntorohebat@gmail.com>
2026-04-20 15:35:11 +0700

    FORCE UPDATE: Change to 3001

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// Paksa menggunakan port 3001 (Langsung ke Backend)
+// Paksa menggunakan alamat domain rootmastr.space
 const API_URL = import.meta.env.VITE_API_URL || 'http://rootmastr.space:3001';
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://rootmastr.space:3001';
 
